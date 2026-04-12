5f2f990c01b7670cf869c66d703c0e0f1a7ea07a
Larissarbnf <larissarbnf@gmail.com>
2026-04-12 13:08:23 -0300

    últimos ajustes dto

---
+++ src/main/java/com/example/exambuilder/web/dto/QuestionAlternativeCreateDto.java
@@ -0,0 +1,19 @@
+package com.example.exambuilder.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class QuestionAlternativeCreateDto {
+
+    @NotBlank
+    private String text;
+}
