5557345f8aee15f42fd8e44d92bf712313b778a5
Larissarbnf <larissarbnf@gmail.com>
2026-04-12 11:50:17 -0300

    enviando

--- src/main/java/com/example/exambuilder/entity/Enum/QuestionType.java
+++ src/main/java/com/example/exambuilder/entity/Enum/QuestionType.java
@@ -1,5 +1,16 @@
 package com.example.exambuilder.entity.Enum;
 
 public enum QuestionType {
+    ABERTA(1),
+    FECHADA(2);
 
+    private final int valor;
+
+    QuestionType(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
--- src/main/java/com/example/exambuilder/web/dto/mapper/QuestionAlternativeMapper.java
+++ src/main/java/com/example/exambuilder/web/dto/mapper/QuestionAlternativeMapper.java
@@ -1,5 +1,17 @@
 package com.example.exambuilder.web.dto.mapper;
 
+import com.example.exambuilder.entity.QuestionAlternative;
+import com.example.exambuilder.web.dto.QuestionAlternativeDto;
+
 public class QuestionAlternativeMapper {
 
+    public static QuestionAlternative toEntity(QuestionAlternativeDto dto) {
+        QuestionAlternative alternative = new QuestionAlternative();
+        alternative.setText(dto.getText());
+        return alternative;
+    }
+
+    public static QuestionAlternativeDto toDto(QuestionAlternative entity) {
+        return new QuestionAlternativeDto(entity.getText());
+    }
 }
